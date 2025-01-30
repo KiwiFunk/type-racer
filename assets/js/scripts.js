85791537075176e15f8f75f5d8321210432664d4
Alex <alex_p97@live.com>
2025-01-30 20:13:08 +0000

    refactored formatText function to be more dynamic

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,23 +5,13 @@ document.addEventListener('DOMContentLoaded', function() {
     for (let optionToggle of functionElements) {
         optionToggle.addEventListener('click', function() {
             this.classList.toggle('selected');
-
-            let optionType = this.getAttribute('data-type');
-            formatText(optionType);
-
+            formatText();
         });
     }
-
-    populateTextArea();
+    formatText();
     getUserInput();
 });
 
-function populateTextArea() {
-    const typeBox = document.getElementById('type-area');
-    const placeholderText = typeBox.querySelector('.placeholder'); 
-    const userTextElement = typeBox.querySelector('.user-text');
-    placeholderText.textContent = generateText();
-}
 
 function generateText() {
     const words = ['the', 'be', 'to', 'of', 'and', 'a', 'in', 'that', 'have', 'I', 
@@ -32,12 +22,16 @@ function generateText() {
         'when', 'which', 'them', 'some', 'me', 'people', 'take', 'out', 'into', 'just'];
 
     let text = '';
+    // Use VW to calculate the number of words to generate
     for (let i = 0; i < 100; i++) {
         text += words[Math.floor(Math.random() * words.length)] + ' ';
     }
     return text.trim();
 }
 
+/**
+ * Handles the user input text.
+ */
 function getUserInput() {
     const typeBox = document.getElementById('type-area');
     const placeholderText = typeBox.querySelector('.placeholder');
@@ -70,91 +64,80 @@ function wrongInput() {
     }, 100);
 }
 
-function formatText(option) {
+/**
+ * Creates a paragraph of text for the user to type.
+ * Formats depending on any selected formatting options.
+ */
 
-    const textContent = generateText();
-
-    switch (option) {
-        case 'punctuation':
-            formatPunctuation(textContent);
-            break;
-        case 'numerical':
-            formatNumbers(textContent);
-            break;
-        default:
-            alert(`Unknown option type: ${option}`);
-            throw `Unknown option type: ${option}. Aborting!`;
-         
-    }
+function formatText() {
+    //Generate New Text and clear User Input
+    let textContent = generateText();
+    document.querySelector('.user-text').textContent = '';
+    //Check selected options
+    let addPunctuation = document.querySelector(`[data-type="punctuation"]`).classList.contains('selected');
+    let addNumbers = document.querySelector(`[data-type="numerical"]`).classList.contains('selected');
+    //Format text
+    if (addPunctuation) {
+        formatPunctuation(textContent);
+        textContent = document.querySelector('.placeholder').textContent;
+    } 
+    if (addNumbers) {
+        formatNumbers(textContent);
+        textContent = document.querySelector('.placeholder').textContent;
+    } 
+    document.querySelector('.placeholder').textContent = textContent;
 }
 
+
+//Text formatting functions
+
 function formatPunctuation(text) {
-    //If punctuation is not selected, generate new text with no formatting.
-    if (!document.querySelector(`[data-type="punctuation"]`).classList.contains('selected')) {
-        document.querySelector('.placeholder').textContent = generateText();
-        return;
-    }
-    else {
-        const words = text.split(' ');              // split paragraph into word array
-        let result = '';                            // variable to hold new string  
-        let shouldCapitalize = true;                // flag to capitalize first word
+    const words = text.split(' ');              // split paragraph into word array
+    let result = '';                            // variable to hold new string  
+    let shouldCapitalize = true;                // flag to capitalize first word
 
-        for (let i = 0; i < words.length; i++) {
+    for (let i = 0; i < words.length; i++) {
 
-            if (shouldCapitalize) {                 // capitalize first word
+        if (shouldCapitalize) {                 // capitalize first word
 
-                words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
-                shouldCapitalize = false;
-            }
-
-            result += words[i];                     // append word to result string
-
-            if (i < words.length - 1) {             // if not the last word
-                if (Math.random() < 0.1) {          // 10% chance of adding a period
-                    result += '. ';
-                    shouldCapitalize = true;        // next word should be capitalized
-                } 
-                else if (Math.random() < 0.05) {     // 5% chance of adding a comma
-                    result += ', ';
-                }
-                else result += ' ';                 // else add a space bewteen words
-            }
-
-            if (i === words.length - 1) {           // if last word
-                result += '.';                      // add a period
-            }
+            words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+            shouldCapitalize = false;
         }
-        document.querySelector('.placeholder').textContent = result;
+
+        result += words[i];                     // append word to result string
+
+        if (i < words.length - 1) {             // if not the last word
+            if (Math.random() < 0.1) {          // 10% chance of adding a period
+                result += '. ';
+                shouldCapitalize = true;        // next word should be capitalized
+            }
+            else if (Math.random() < 0.05) {     // 5% chance of adding a comma
+                result += ', ';
+            }
+            else result += ' ';                 // else add a space bewteen words
+        }
+
+        if (i === words.length - 1) {           // if last word
+            result += '.';                      // add a period
+        }
     }
+    document.querySelector('.placeholder').textContent = result;
 }
 
 function formatNumbers(text) {
-    if (!document.querySelector(`[data-type="numerical"]`).classList.contains('selected')) {
-        document.querySelector('.placeholder').textContent = generateText();
-        return;
-    }
-    else {
-        const words = text.split(' ');
-        let result = '';
+    const words = text.split(' ');
+    let result = '';
 
-        for (let i = 0; i < words.length; i++) {
-            result += words[i];
-            
-            if (Math.random() < 0.04) {                                     // 4% chance to add a number
-                const numLength = Math.floor(Math.random() * 3) + 3;        // random length 3-5
-                const num = Math.floor(Math.random() * Math.pow(10, numLength));
-                result += ' ' + num;
-            }
+    for (let i = 0; i < words.length; i++) {
+        result += words[i];
 
-            if (i < words.length - 1) {
-                result += ' ';
-            }
+        if (Math.random() < 0.04) {                                     // 4% chance to add a number
+            const numLength = Math.floor(Math.random() * 3) + 3;        // random length 3-5
+            const num = Math.floor(Math.random() * Math.pow(10, numLength));
+            result += ' ' + num;
         }
 
-        document.querySelector('.placeholder').textContent = result;
-
+        if (i < words.length - 1) result += ' ';
     }
+    document.querySelector('.placeholder').textContent = result;
 }
-
-//call function to see which functions are selected
-//call function for each function selected
