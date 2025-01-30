4add06f4dc11a72ea0b6d0a6d5223fabbbeaffaf
Alex <alex_p97@live.com>
2025-01-30 16:05:33 +0000

    Finished basic logic for taking user input and checking against placeholder text

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -21,6 +21,34 @@ function populateTextArea() {
 
 function getUserInput() {
     const typeBox = document.getElementById('type-area');
+    const placeholderText = typeBox.querySelector('.placeholder');
     const userTextElement = typeBox.querySelector('.user-text');
-    userTextElement.textContent = 'Hello Wo';
+    userTextElement.textContent = '';
+
+    document.addEventListener('keydown', function(e) {
+        const targetText = placeholderText.textContent;
+        const currentText = userTextElement.textContent;
+        
+        if (!e.ctrlKey && !e.altKey && !e.metaKey && e.key.length === 1) {
+            if (currentText.length < targetText.length && e.key === targetText[currentText.length]) {
+                userTextElement.textContent += e.key;
+            }
+            
+            else if (e.key === 'Backspace') {
+                userTextElement.textContent = userTextElement.textContent.slice(0, -1);
+            }
+
+            else wrongInput();
+        }
+    });
+}
+
+function wrongInput() {
+    const userTextElement = document.querySelector('.user-text');
+    const originalColor = userTextElement.style.color;
+    userTextElement.style.color = 'var(--font-color-wrong)';
+    setTimeout(() => {
+        userTextElement.style.color = originalColor;
+    }, 100);
+
 }
